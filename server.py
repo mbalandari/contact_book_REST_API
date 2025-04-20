c13d0e01574ab53290ba5db0d27ace6549e4f97d
codingMaz <m.balandari.toroghi@gmail.com>
2025-04-20 10:29:59 +0330

    create a sample daatset to use and mock data for testing the api

--- server.py
+++ server.py
@@ -1,3 +1,13 @@
 from flask import Flask, request
 
 app = Flask(__name__)
+
+
+contacts = [
+    {"name": "Maz", "phone": "123-456-7890", "id": "1"},
+    {"name": "Sarah", "phone": "547-456-7890", "id": "2"},
+    {"name": "John", "phone": "463-456-7890", "id": "3"},
+    {"name": "Nancy", "phone": "999-456-7890", "id": "4"},
+]
+
+next_id = 5
