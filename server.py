142e1f070c9703e32058e654e5cfd50955760c90
codingMaz <m.balandari.toroghi@gmail.com>
2025-04-20 11:01:31 +0330

    add two Read parts from cRud

--- server.py
+++ server.py
@@ -19,5 +19,21 @@ def home():
     return "<h1 style='color:green'>Contact List</h1> <hr> </br> <a href='/contacts'>The list</a>"
 
 
+# cRud
+@app.get("/contacts")
+def list_contacts():
+    return contacts
+
+
+# cRud
+@app.get("/contacts/<id>")
+def read_single_contact(id):
+    for contact in contacts:
+        if contact["id"] == id:
+            return contact
+
+    return "That contact does not exist!"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
